18c0fc09b2aec39d780b6b0dfc20fe1d92560116
shaggyalok24 <shaggralok24@gmail.com>
2024-08-03 23:37:13 +0530

    first tf

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ resource "aws_instance" "my_ec2" {
     ami = "ami-06489866022e12a14"
     instance_type = "t2.micro"
     key_name = "gopal"
-    vpc_security_group_ids = [aws_security_group.demo-sg.id]
     tags = {
 
         name = "alok"
