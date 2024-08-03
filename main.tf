d7b2ce0705e02f5f86dd10ad3d331326668f4ec0
shaggyalok24 <shaggralok24@gmail.com>
2024-08-03 23:31:02 +0530

    first tf

--- main.tf
+++ main.tf
@@ -6,5 +6,18 @@ resource "aws_instance" "my_ec2" {
     ami = "ami-06489866022e12a14"
     instance_type = "t2.micro"
     key_name = "gopal"
+    vpc_security_group_ids = [aws_security_group.demo-sg.id]
+    tags = {
+
+        name = "alok"
+        value = 1
+    }
 }
 
+resource "aws_s3_bucket" "terabucket" {
+
+ bucket = "terabucket" 
+ tags = {
+    name = "practice"
+ }
+}
