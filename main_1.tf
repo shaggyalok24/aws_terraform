c0f8680fbac315ece5b21bf7c2f2e57b19bb5e8a
shaggyalok24 <shaggralok24@gmail.com>
2024-08-11 00:04:57 +0530

    second tf

--- main_1.tf
+++ main_1.tf
@@ -10,3 +10,16 @@ data "aws_availability_zones" "available" {
 output "list_of_az" {
   value = data.aws_availability_zones.available[*].names
 }
+
+resource "aws_instance" "my_ec2" {
+    for_each = toset( data.aws_availability_zones.available[*].names )
+    ami = var.image.default
+    availability_zone = ${each.key}
+    instance_type = var.instance_type.default
+    key_name = "gopal"
+    tags = {
+
+        name = "alok"
+        value = 
+    }
+}
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-#variable "instance_type" {
-#     default = "t2.micro"
-#}
+variable "instance_type" {
+     default = "t2.micro"
+}
 
-#variable "image" {
-#     default = ami-0ad21ae1d0696ad58
-#}
+variable "image" {
+     default = "ami-0ad21ae1d0696ad58"
+}
