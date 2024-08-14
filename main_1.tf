3b640e583b742674b2f8b09d792ebba3ccb29858
shaggyalok24 <42790157+shaggyalok24@users.noreply.github.com>
2024-08-14 07:49:09 +0530

    Update main_1.tf

--- main_1.tf
+++ main_1.tf
@@ -14,7 +14,7 @@ output "list_of_az" {
 resource "aws_instance" "my_ec2" {
     for_each = toset( data.aws_availability_zones.available[*].names )
     ami = var.image.default
-    availability_zone = ${each.key}
+    availability_zone = each.key
     instance_type = var.instance_type.default
     key_name = "gopal"
     tags = {
