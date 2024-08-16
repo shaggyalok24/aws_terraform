88a9ca6bd5327cd545fe0e2691100ae7a7c07067
shaggyalok24 <42790157+shaggyalok24@users.noreply.github.com>
2024-08-16 23:34:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-Variable "instance_type" {
+variable "instance_type" {
      type = string
      default = "t2.micro"
 }
 
-Variable "image" {
+variable "image" {
      type = string
      default = "ami-0ad21ae1d0696ad58"
 }
