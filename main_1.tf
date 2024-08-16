2223579aa0bdde9dea216378ce61a1f864053309
shaggyalok24 <42790157+shaggyalok24@users.noreply.github.com>
2024-08-16 10:45:14 +0530

    Update main_1.tf

--- main_1.tf
+++ main_1.tf
@@ -8,18 +8,18 @@ data "aws_availability_zones" "available" {
 }
 
 output "list_of_az" {
-  value = toset( data.aws_availability_zones.available.name )
+  value = data.aws_availability_zones.available.names
 }
 
 resource "aws_instance" "my_ec2" {
-    for_each = tomap( data.aws_availability_zones.available.name )
+    count = length( data.aws_availability_zones.available.names )
     ami = var.image.default
-    availability_zone = each.key
+    availability_zone = element(data.aws_availability_zones.available.names,count.index)
     instance_type = var.instance_type.default
     key_name = "gopal"
     tags = {
 
         name = "alok"
-        value = each.key
+        value = "instance-${element(data.aws_availability_zones.available.names,count.index)}"
     }
 }
