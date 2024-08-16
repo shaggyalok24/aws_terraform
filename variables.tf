9488c244b7cd11f1cc617a1999289632ad4c1122
shaggyalok24 <42790157+shaggyalok24@users.noreply.github.com>
2024-08-16 13:08:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "instance_type" {
-     default = "t2.micro"
+     default = t2.micro
 }
 
 variable "image" {
-     default = "ami-0ad21ae1d0696ad58"
+     default = ami-0ad21ae1d0696ad58
 }
