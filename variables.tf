0baca3f2b9eec7f311fe1b5002367895a7e6e73c
shaggyalok24 <shaggralok24@gmail.com>
2024-08-18 18:43:01 +0530

    vpc tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,16 @@ variable "image" {
      type = string
      default = "ami-0ad21ae1d0696ad58"
 }
+
+variable "public_subnet_cidr" {
+     type = list(string)
+     description = "Public Subnet"
+     default = ["10.0.0.0/25","10.0.0.128/25"]
+       
+}
+
+variable "azs" {
+     type = list(string)
+     default = [ "ap-south-1a", "ap-south-1b" ]
+
+}
---
+++ vpc.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/24"
+    tags = {
+      name = "demo vpc"
+    }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count = length(var.public_subnet_cidr, count.index)
+  vpc_id = aws_vpc.main.id
+  cidr_block = element(var.public_subnet_cidr, count.index)
+  availability_zone = element(var.azs, count.index)
+}
