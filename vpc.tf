5e70fa0e0d84b215ac05e0c4c30dd8d442591359
shaggyalok24 <shaggralok24@gmail.com>
2024-08-18 18:46:40 +0530

    vpc tf

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  count = length(var.public_subnet_cidr, count.index)
+  count = length(var.public_subnet_cidr)
   vpc_id = aws_vpc.main.id
   cidr_block = element(var.public_subnet_cidr, count.index)
   availability_zone = element(var.azs, count.index)
